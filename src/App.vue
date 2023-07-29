bb3d972c747aef9040dba05e9f9873a68cd8e264
nsquishpe <noeliaquishpe@hotmail.com>
2023-07-29 13:39:42 -0500

    Mensaje del commit

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,20 @@
 <script setup></script>
 
 <template>
-    <router-view />
-</template>
+    <div>
+      <!-- Contenido de la página de inicio de sesión (Login.vue) -->
+      <router-view></router-view>
+    </div>
+  </template>
+
+<script>
+export default {
+  name: 'App',
+  created() {
+    // Redirige a la página de inicio de sesión al iniciar la aplicación
+    this.$router.push({ name: 'login' });
+  }
+};
+</script>
 
 <style scoped></style>
